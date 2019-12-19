c98445667ea66bdee310916b498b04d4655c3194
juanpjpr <juanp_jpr@hotmail.com>
2019-12-19 17:58:55 -0300

    Add files via upload

---
+++ parser.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    int l1 = 3;
+    int l2 = A;
+
+    printf("%d\n", isxdigit(l1));
+    printf("%d\n", isxdigit(l2));
+
+    getchar();
+}
